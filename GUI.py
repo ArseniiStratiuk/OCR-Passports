6ca3ac3bdbc160c02317dde2d02d8f575aa88971
ArseniiStratiuk <104721499+ArseniiStratiuk@users.noreply.github.com>
2022-12-13 14:09:44 +0200

    Add icon

--- GUI.py
+++ GUI.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+import os
 import sys
 
 import customtkinter as ctk
+from PIL import Image
 
 ctk.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue".
 ctk.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light".
@@ -19,6 +21,8 @@ class Window(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
+        self.iconbitmap(os.path.join(sys.path[0]+"\Icons", "Check_MRZ_Light.ico"))
+        
         self.WINDOW_CENTERING_X = int(self.winfo_screenwidth()/2 - self.WIDTH/2)
         self.WINDOW_CENTERING_Y = int(self.winfo_screenheight()/2 - self.HEIGHT/2)
         
@@ -29,6 +33,26 @@ class Window(ctk.CTk):
         self.protocol(
             "WM_DELETE_WINDOW", self.on_closing
         )  # Call .on_closing() when app gets closed
+        
+        self.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        self.grid_columnconfigure((0, 1, 2), weight=1)
+        
+        self.create_widgets()
+        
+    def create_widgets(self):
+        """
+        Fill the window with widgets.
+        """
+        self.label_1 = ctk.CTkLabel(self, text="Оптичне розпізнавання\nсимволів Вашого\nпаспорта", 
+                                    font=self.LARGE_FONT)
+        self.label_1.grid(row=0, column=0, padx=10, pady=20)
+        
+        self.label_2 = ctk.CTkLabel(self, text="Оберіть фотографію\nпаспорта", 
+                                    font=self.LARGE_FONT)
+        self.label_2.grid(row=0, column=3, padx=10, pady=20)
+        
+        self.button = ctk.CTkButton(self, text="", width=75, height=70, corner_radius=10, 
+                                    )
         
     def on_closing(self, event=0):
         self.destroy()
