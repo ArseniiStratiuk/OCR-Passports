27e778b694b164a2754aad3e286f88514878eae2
ArseniiStratiuk <104721499+ArseniiStratiuk@users.noreply.github.com>
2022-12-13 15:04:08 +0200

    Add images

--- GUI.py
+++ GUI.py
@@ -14,19 +14,33 @@ class Window(ctk.CTk):
     WIDTH = 1242
     HEIGHT = 720
     
-    LARGE_FONT = ("Calibri", 24, "bold")
-    MEDIUM_FONT = ("Calibri", 18, "bold")
-    SMALL_FONT = ("Calibri", 14, "bold")
+    LARGE_FONT = ("Calibri", 34)
+    MEDIUM_FONT = ("Calibri", 28)
+    SMALL_FONT = ("Calibri", 20)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
+        # Upload images that will be used in the program.
+        self.ARROW_DARK = Image.open(os.path.join(sys.path[0]+"\Icons", "Arrow_Dark.png"))
+        self.ARROW_LIGHT = Image.open(os.path.join(sys.path[0]+"\Icons", "Arrow_Light.png"))
+        self.arrow = ctk.CTkImage(self.ARROW_DARK, self.ARROW_LIGHT, (100, 100))
+        
+        self.CHECK_MRZ_DARK = Image.open(os.path.join(sys.path[0]+"\Icons", "Check_MRZ_Dark.png"))
+        self.CHECK_MRZ_LIGHT = Image.open(os.path.join(sys.path[0]+"\Icons", "Check_MRZ_Light.png"))
+        self.check_mrz = ctk.CTkImage(self.CHECK_MRZ_DARK, self.CHECK_MRZ_LIGHT, (100, 100))
+        
+        self.LOADING_DARK = Image.open(os.path.join(sys.path[0]+"\Icons", "Loading_Dark.png"))
+        self.LOADING_LIGHT = Image.open(os.path.join(sys.path[0]+"\Icons", "Loading_Light.png"))
+        self.loading_image = ctk.CTkImage(self.LOADING_DARK, self.LOADING_LIGHT, (100, 100))
+        # --------------------------------------------------
         
         self.iconbitmap(os.path.join(sys.path[0]+"\Icons", "Check_MRZ_Light.ico"))
         
         self.WINDOW_CENTERING_X = int(self.winfo_screenwidth()/2 - self.WIDTH/2)
         self.WINDOW_CENTERING_Y = int(self.winfo_screenheight()/2 - self.HEIGHT/2)
         
-        self.title("OCR Passport")
+        self.title("OCR Your Passport")
         self.geometry(
             f"{self.WIDTH}x{self.HEIGHT}+{self.WINDOW_CENTERING_X}+{self.WINDOW_CENTERING_Y}"
         )
@@ -49,10 +63,15 @@ class Window(ctk.CTk):
         
         self.label_2 = ctk.CTkLabel(self, text="Оберіть фотографію\nпаспорта", 
                                     font=self.LARGE_FONT)
-        self.label_2.grid(row=0, column=3, padx=10, pady=20)
+        self.label_2.grid(row=0, column=2, padx=10, pady=20)
         
-        self.button = ctk.CTkButton(self, text="", width=75, height=70, corner_radius=10, 
-                                    )
+        self.button = ctk.CTkButton(self, text="", width=85, height=75, corner_radius=10, 
+                                    image=self.check_mrz)
+        self.button.grid(row=1, column=2, padx=10, pady=20)
+        
+        self.loading = ctk.CTkLabel(self, text="Результати\nзавантажуються...", compound="left", 
+                                    font=self.SMALL_FONT, image=self.loading_image)
+        self.loading.grid(row=1, column=0, padx=10, pady=20)
         
     def on_closing(self, event=0):
         self.destroy()
